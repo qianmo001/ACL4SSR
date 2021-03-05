4344fef7b4629e88fbcf746822937f6ee2f6201f
qianmo001 <55676895+qianmo001@users.noreply.github.com>
2021-03-05 18:26:33 +0800

    Update 69sp.js

--- 69sp.js
+++ 69sp.js
@@ -1,5 +1,14 @@
-var body = $response.body; // 声明一个变量body并以响应消息体赋值
-var obj = JSON.parse(body); // JSON.parse()将json形式的body转变成对象处理
+/*QQ群528680197
+转载请注明出处
+圈x
+[rewrite_local]
+#69视频解锁会员
+^http:\/\/69shipin\.vip\/api(\/community|\/user)(\/edit|\/personal)  url script-response-body https://raw.githubusercontent.com/qianmo001/ACL4SSR/rm/69sp.js
+[mitm]
+hostname= 69shipin.vip
+*/
+var body = $response.body; 
+var obj = JSON.parse(body); 
 
 obj.data.vip_time="2032-01-01 00:00:00";
 obj.data.username="❤️小白不白❤️";
@@ -8,5 +17,5 @@ obj.data.vip=1;
 obj.data.vip_ymd="2032-01-01";
 obj.code=200;
 
-body = JSON.stringify(obj); // 重新打包回json字符串
-$done(body); // 结束修改
+body = JSON.stringify(obj);
+$done(body); 
