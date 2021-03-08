9e6890fae70fae6283c75b2957801194f2ab9a61
qianmo001 <55676895+qianmo001@users.noreply.github.com>
2021-03-08 17:28:51 +0800

    Update xxmhvip.js

--- xxmhvip.js
+++ xxmhvip.js
@@ -4,7 +4,7 @@
 圈x
 [rewrite_local]
 #羞羞漫画解锁会员
-^http:\/\/c\.banghankeji\.com\/common\/MemberInfo\/getEntityByCode  url script-response-body https://raw.githubusercontent.com/qianmo001/ACL4SSR/rm/xxmh.js
+^http:\/\/c\.banghankeji\.com\/common\/MemberInfo\/getEntityByCode  url script-response-body https://raw.githubusercontent.com/qianmo001/ACL4SSR/rm/xxmhvip.js
 hostname= c.banghankeji.com
 下载地址：商店搜索羞羞漫画即可
 使用方法：需要登录才可破解，未登录无法破解
