da6cc81fc001f7fa6c3cf7f256f57505c129072c
qianmo001 <55676895+qianmo001@users.noreply.github.com>
2021-03-08 16:09:19 +0800

    Update xxmh.js

--- xxmh.js
+++ xxmh.js
@@ -4,9 +4,8 @@
 圈x
 [rewrite_local]
 #羞羞漫画解锁会员
-^http:\/\/c.banghankeji\.com\/common\/MemberInfo\/getEntityByCode  url script-response-body https://raw.githubusercontent.com/qianmo001/ACL4SSR/rm/69sp.js
-[mitm]
-hostname= 69shipin.vip
+^http:\/\/c\.banghankeji\.com\/common\/MemberInfo\/getEntityByCode  url script-response-body https://raw.githubusercontent.com/qianmo001/ACL4SSR/rm/xxmh.js
+hostname= c.banghankeji.com
 下载地址：商店搜索羞羞漫画即可
 使用方法：需要登录才可破解，未登录无法破解
 */
