82a31a9680113837e399ab8cfba1203a5708d3cf
qianmo001 <55676895+qianmo001@users.noreply.github.com>
2021-03-10 01:16:43 +0800

    Update xlsp.js

--- xlsp.js
+++ xlsp.js
@@ -1,16 +1,43 @@
-/*
-^https:\/\/*\.pearkin\.com\/api\/account\/IndexDetail  url script-response-body https://raw.githubusercontent.com/qianmo001/ACL4SSR/rm/xlsp.js
 
-*.pearkin.com
+/*
+By:❤小白不白❤
+QQ群528680197
+转载请注明出处
+圈x
+[rewrite_local]
+#雪梨解锁会员
+^https:\/\/m\.pearkin\.com\/api(\/account|\/movie)(\/IndexDetail|\/IsVip|\/WatchMovieNew) https://raw.githubusercontent.com/qianmo001/ACL4SSR/rm/xlsp.js
+[mitm]
+hostname= m.pearkin.com
 */
 
-var body = $response.body; // 声明一个变量body并以响应消息体赋值
-var obj = JSON.parse(body); // JSON.parse()将json形式的body转变成对象处理
+https://m.pearkin.com/api/movie/WatchMovieNew
 
-obj.vipLevel=5;
-obj.vipEndTime="2099-03-09";
-obj.cartoonVip=true;
-obj.nickName="️小白不白️"
+var body = $response.body;//声明变量
+var url = $request.url;
+var obj = JSON.parse(body);
 
-body = JSON.stringify(obj); // 重新打包回json字符串
-$done(body); // 结束修改;
+const vip = '/api/account/IndexDetail';
+
+const isvip = '/api/account/IsVip';
+const zwzm = '/api/movie/WatchMovieNew';
+if (url.indexOf(vip) != -1) {
+  obj.nickName = "️小白不白️";
+  obj.vipEndTime = "2099-03-09";
+  obj.cartoonVip = true;
+  obj.vipLevel = 101;
+  obj.cartoonVipEndTime = "2099-03-09";
+ body = JSON.stringify(obj);
+ }
+
+if (url.indexOf(isvip) != -1) {
+  obj.value = true;
+ body = JSON.stringify(obj);
+ }
+ 
+ if (url.indexOf(zwzm) != -1) {
+  obj.canWath = true;
+ body = JSON.stringify(obj);
+ }
+
+$done({body});
