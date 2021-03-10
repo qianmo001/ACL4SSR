559177f3c2563842ec4a2465d0ae2fe6ebd13d38
qianmo001 <55676895+qianmo001@users.noreply.github.com>
2021-03-10 11:45:46 +0800

    Update xlsp.js

--- xlsp.js
+++ xlsp.js
@@ -9,9 +9,8 @@ QQ群528680197
 ^https:\/\/m\.pearkin\.com\/api(\/account|\/movie)(\/IndexDetail|\/IsVip|\/WatchMovieNew)  url script-response-body  https://raw.githubusercontent.com/qianmo001/xiaobaibubai/rm/xlsp.js
 [mitm]
 hostname= m.pearkin.com
+破解内容：视频无线看，有问题加群反馈
 */
-
-https://m.pearkin.com/api/movie/WatchMovieNew
 
 var body = $response.body;
 var url = $request.url;
@@ -19,10 +18,16 @@ var obj = JSON.parse(body);
 
 const vip = '/api/account/IndexDetail';
 
+const dspq = '/api/video/watch/';
+
 const isvip = '/api/account/IsVip';
+
 const zwzm = '/api/movie/WatchMovieNew';
+
+const cv = '/api/Account/CheckVip';
+
 if (url.indexOf(vip) != -1) {
-  obj.nickName = "️❤小白不白️❤";
+  obj.nickName = "️小白不白️";
   obj.vipEndTime = "2099-03-09";
   obj.cartoonVip = true;
   obj.vipLevel = 101;
@@ -37,6 +42,18 @@ if (url.indexOf(isvip) != -1) {
  
  if (url.indexOf(zwzm) != -1) {
   obj.canWath = true;
+ body = JSON.stringify(obj);
+ }
+
+if (url.indexOf(dspq) != -1) {
+  obj.value = true;
+  obj.message = 0;
+ body = JSON.stringify(obj);
+ }
+
+if (url.indexOf(cv) != -1) {
+  obj.value = true;
+  obj.message = 0;
  body = JSON.stringify(obj);
  }
 
